4e233552efdb35e1b74159dfb982e05a946fe2a1
solyxassistencia-a11y <solyxassistencia@gmail.com>
2026-08-19 15:02:20 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,47 @@
-const CACHE_NAME = "AppFidelidade-v13"; // Mudamos para v11 para forçar a atualização!
-const OFFLINE_URL = "offline.html"; // Sem a barra (/) no início!
+const CACHE_NAME = "AppFidelidade-v14";
+const OFFLINE_URL = "./offline.html";
 
 const urlsToCache = [
-  "./", // Ponto e barra significa "A página atual (index)"
-  OFFLINE_URL
+    "./",
+    "./offline.html"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-          // Salva a página principal e o Quiz na memória
-          return cache.addAll(urlsToCache);
-      })
-      .catch(err => console.error("Erro ao fazer cache dos arquivos:", err))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request).catch(() => {
-            // Se a internet cair e for uma navegação de página
-            if (event.request.mode === 'navigate') {
-                return caches.match(OFFLINE_URL);
-            }
-        });
-      })
-  );
+
+    if (event.request.mode === "navigate") {
+
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => caches.match(OFFLINE_URL))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
