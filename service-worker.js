502885ae08f5f203703ca601840ced5dc7459362
solyxassistencia-a11y <solyxassistencia@gmail.com>
2026-08-21 13:40:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,8 @@ const ARQUIVOS_PARA_CACHEAR = [
   './fid_game1.html',
   './fid_game2.html',
   './fid_game3.html',
+  './fid_game4.html',
+  './fid_game5.html',
   './fid_game4.html'
   // Quando quiser adicionar novos jogos no futuro, basta colocar o nome do arquivo aqui!
 ];
