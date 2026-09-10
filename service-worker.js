09f8ac4b74fdc7140a49fe8bf44d8da5d5e029fb
solyxassistencia-a11y <solyxassistencia@gmail.com>
2026-09-09 23:02:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 /* ============================================================
    1. CONFIGURAÇÃO
    ============================================================ */
-const CACHE_NAME = "AppFidelidade-v31";
+const CACHE_NAME = "AppFidelidade-v32";
 const OFFLINE_PAGE = "./offline.html";
 /*
  * Arquivos que devem existir obrigatoriamente
